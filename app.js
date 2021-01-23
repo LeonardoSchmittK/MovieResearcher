2d6e01105e5b5df9314168cbaf1e3c1068f7d1ef
Leonardo <Leoeyeschmittk@gmail.com>
2021-01-23 01:17:03 -0300

    Creating a server and static directory as public

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const path = require("path");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+	res.end(homePage);
+	res.sendFile(path.resolve(__dirname, "index.html"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+	console.log(`Running on port ${port}`);
+});
---
+++ public/script/firebaseConfig.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyDlU_-zDAwF5iUHi_kALSzaNau6irMdIBM",
+	authDomain: "movie-researcher.firebaseapp.com",
+	projectId: "movie-researcher",
+	storageBucket: "movie-researcher.appspot.com",
+	messagingSenderId: "232594490220",
+	appId: "1:232594490220:web:1434682a397c3e8727e949",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
